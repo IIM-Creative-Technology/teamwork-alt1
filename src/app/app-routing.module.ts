000c37331b08d153f09b64e667a61f1a8ea3b3c6
Matthis Blondin <matthis.blondin@edu.devinci.fr>
2021-09-22 14:42:47 +0100

    ADD feature/krilin as a hero

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BatmanComponent } from './batman/batman.component';
+import { KrilinComponent } from './krilin/krilin.component';
 
 import { HeroesComponent } from './heroes/heroes.component';
 import { PoisonJulyComponent } from './poison-july/poison-july.component';
@@ -10,7 +11,8 @@ const routes: Routes = [
   { path: '', redirectTo: '/heroes', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
   { path: 'detail/poison-july', component: PoisonJulyComponent },
-  { path: "detail/batman", component:BatmanComponent }
+  { path: 'detail/batman', component: BatmanComponent },
+  { path: 'detail/krilin', component: KrilinComponent }
 ];
 
 @NgModule({
